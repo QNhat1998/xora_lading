ca53f8f9fb3a70e0c4c2c4fcda917a14f2cf53bf
Nhat Do <doquangnhat0809@gmail.com>
2025-02-08 03:23:54 +0700

    project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
+
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features/>
+      <Pricing/>
+    </main>
+  );
+}
+
+export default App;
